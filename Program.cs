4fb0b871eaee0860295ae3bcb322d23245f6345e
Junior Colares <juniorcolares6@gmail.com>
2022-10-16 11:59:06 -0300

    started creating home page

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
+using System.Net.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
-app.MapGet("/alo", () =>
+app.MapGet("/api/search/{name}", (string name) =>
 {
-    return Results.Ok();
+    HttpClient client = new HttpClient();
+    var response = client.GetStringAsync($"https://www.googleapis.com/youtube/v3/search?part=snippet&q={name}");
+    return Results.Ok(response);
 });
 
 app.Run();
---
+++ client/src/Pages/Home/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './home.css';
+
+function Home() {
+    return (
+        <>
+        <h1>Music Downloader</h1>
+        <h2>Search for songs you like and download them</h2>
+        </>
+    )
+}
+
+export default Home;
