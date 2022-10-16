48e061cc0b64f4e73faca9abbe7c96cfad7b4cb0
spyvanilla <levisbarros1234@gmail.com>
2022-10-16 15:18:22 -0300

    finished first route

--- Program.cs
+++ Program.cs
@@ -1,15 +1,30 @@
 using System.Net.Http;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
-app.MapGet("/api/search/{name}", (string name) =>
+string GetApiKey()
+{
+    using (StreamReader r = new StreamReader("credentials.json"))
+    {
+        string json = r.ReadToEnd();
+        dynamic items = JsonConvert.DeserializeObject(json);
+        string apiKey = items["API_KEY"];
+        return apiKey;        
+    }
+}
+
+string apiKey = GetApiKey();
+
+app.MapGet("/api/search/{name}", async (string name) =>
 {
     HttpClient client = new HttpClient();
-    var response = client.GetStringAsync($"https://www.googleapis.com/youtube/v3/search?part=snippet&q={name}");
-    return Results.Ok(response);
+    var response = client.GetStringAsync($"https://www.googleapis.com/youtube/v3/search?key={apiKey}&part=snippet&q={name}");
+    var data = await response;
+    return Results.Ok(data);
 });
 
 app.Run();
