e99ebe0cb22af60eed702d83d47a280f865e2b4b
spyvanilla <levisbarros1234@gmail.com>
2022-10-19 14:34:12 -0300

    changed download video component styling

--- client/src/Components/DownloadVideo.tsx
+++ client/src/Components/DownloadVideo.tsx
@@ -4,6 +4,7 @@ import Axios from 'axios';
 import fileDownload from 'js-file-download';
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faDownload} from '@fortawesome/free-solid-svg-icons';
 import {faYoutube} from '@fortawesome/free-brands-svg-icons';
 
 function DownloadVideo({chosenVideo, setChosenVideo} : {chosenVideo: any, setChosenVideo: React.Dispatch<any>}) {
@@ -13,12 +14,12 @@ function DownloadVideo({chosenVideo, setChosenVideo} : {chosenVideo: any, setCho
     }
 
     return (
-        <div className="video" style={{marginTop: "50px"}}>
+        <div className="download-video" style={{marginTop: "50px"}}>
             <img src={chosenVideo.image} alt={chosenVideo.title} className="video-image"></img>
             <h3>{chosenVideo.title}</h3>
             <a href={chosenVideo.href} target="_blank" rel="noreferrer" className="open-video"><i><FontAwesomeIcon icon={faYoutube} /></i> Open video</a>
-            <button onClick={handleClick}>Download</button>
-            <button onClick={() => setChosenVideo(null)}>Cancel</button>
+            <button onClick={handleClick} id="download-button">{<FontAwesomeIcon icon={faDownload} />}</button>
+            <button onClick={() => setChosenVideo(null)} id="cancel-button">Cancel</button>
         </div>
     )
 }
